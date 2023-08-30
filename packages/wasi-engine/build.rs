1100d8e2358322f4ee823b7c47b430e7714d3883
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-08-30 17:36:24 +0200

    feat(wasi-engine): use new wasi http context

--- packages/wasi-engine/build.rs
+++ packages/wasi-engine/build.rs
@@ -1,39 +1,21 @@
 use std::{collections::HashMap, fs, io::Write, path::PathBuf};
-use wit_component::ComponentEncoder;
 
 fn main() {
     let current_dir = std::env::current_dir().unwrap();
     let target_dir = current_dir.join("../../target");
-    // we first read the bytes of the wasm module.
-    // let module_path = target_dir.join("wasm32-wasi/release/aws_cli.wasi.wasm");
-    // "/Users/admin/projects/public/smithy-rs/target/wasm32-wasi/release/webassembly.wasi.wasm";
-    // let module = fs::read(&module_path).unwrap();
-    // let adapter_path = "./wasi_snapshot_preview1.wasm";
-    // let wasi_adapter = fs::read(adapter_path).unwrap();
-    // // then we transform module to component.
-    // // remember to get wasi_snapshot_preview1.wasm first.
-    // let component = ComponentEncoder::default()
-    //     .module(module.as_slice())
-    //     .unwrap()
-    //     .validate(true)
-    //     .adapter("wasi_snapshot_preview1", &wasi_adapter)
-    //     .unwrap()
-    //     .encode()
-    //     .unwrap();
-    let component_path = target_dir.join("wasm32-wasi/debug/aws_cli.wasm");
+    let component_path = target_dir.join("wasm32-wasi/release/aws_cli.wasm");
     let component = fs::read(&component_path).unwrap();
-    // fs::write(&component_path, &component).unwrap();
 
     let import_map = HashMap::from([
-        ("wasi:cli/*".into(), "cliBase#*".into()),
-        ("wasi:cli-base/*".into(), "cliBase#*".into()),
+        ("wasi:cli/*".into(), "cli#*".into()),
         ("wasi:clocks/*".into(), "clocks#*".into()),
         ("wasi:filesystem/*".into(), "filesystem#*".into()),
         ("wasi:http/*".into(), "http#*".into()),
+        ("wasi:logging/*".into(), "logging#*".into()),
         ("wasi:io/*".into(), "io#*".into()),
         ("wasi:poll/*".into(), "poll#*".into()),
         ("wasi:random/*".into(), "random#*".into()),
-        // ("wasi:sockets/*".into(), "sockets#*".into()),
+        ("wasi:sockets/*".into(), "sockets#*".into()),
     ]);
     let opts = js_component_bindgen::TranspileOpts {
         name: "aws".to_string(),
--- packages/wasi-engine/src/main.rs
+++ packages/wasi-engine/src/main.rs
@@ -1,15 +1,14 @@
-use std::{any::Any, env, pin::Pin};
 use wasmtime::{
     component::{Component, Linker},
     Config, Engine, Store,
 };
-use wasmtime_wasi::preview2::{wasi::command::Command, Table, WasiCtx, WasiCtxBuilder, WasiView};
-use wasmtime_wasi_http::WasiHttp;
+use wasmtime_wasi::preview2::{command::Command, Table, WasiCtx, WasiCtxBuilder, WasiView};
+use wasmtime_wasi_http::{WasiHttpCtx, WasiHttpView};
 
 pub struct Ctx {
     table: Table,
     wasi: WasiCtx,
-    http: WasiHttp,
+    http: WasiHttpCtx,
 }
 
 impl WasiView for Ctx {
@@ -27,76 +26,43 @@ impl WasiView for Ctx {
     }
 }
 
-pub fn add_to_linker<T: WasiView>(l: &mut wasmtime::component::Linker<T>) -> anyhow::Result<()> {
-    wasmtime_wasi::preview2::wasi::clocks::wall_clock::add_to_linker(l, |t| t)?;
-    wasmtime_wasi::preview2::wasi::clocks::monotonic_clock::add_to_linker(l, |t| t)?;
-    wasmtime_wasi::preview2::wasi::clocks::timezone::add_to_linker(l, |t| t)?;
-    wasmtime_wasi::preview2::wasi::filesystem::filesystem::add_to_linker(l, |t| t)?;
-    // wasmtime_wasi::preview2::wasi::poll::poll::add_to_linker(l, |t| t)?;
-    // wasmtime_wasi::preview2::wasi::io::streams::add_to_linker(l, |t| t)?;
-    wasmtime_wasi::preview2::wasi::random::random::add_to_linker(l, |t| t)?;
-    wasmtime_wasi::preview2::wasi::cli_base::exit::add_to_linker(l, |t| t)?;
-    wasmtime_wasi::preview2::wasi::cli_base::environment::add_to_linker(l, |t| t)?;
-    wasmtime_wasi::preview2::wasi::cli_base::preopens::add_to_linker(l, |t| t)?;
-    wasmtime_wasi::preview2::wasi::cli_base::stdin::add_to_linker(l, |t| t)?;
-    wasmtime_wasi::preview2::wasi::cli_base::stdout::add_to_linker(l, |t| t)?;
-    wasmtime_wasi::preview2::wasi::cli_base::stderr::add_to_linker(l, |t| t)?;
-    Ok(())
+impl WasiHttpView for Ctx {
+    fn http_ctx(&self) -> &WasiHttpCtx {
+        &self.http
+    }
+    fn http_ctx_mut(&mut self) -> &mut WasiHttpCtx {
+        &mut self.http
+    }
 }
 
-// struct WritePipe(wasi_common::pipe::WritePipe<std::io::Sink>);
-
-// impl common::OutputStream for WritePipe {
-//     fn as_any(&self) -> &dyn Any {
-//         self
-//     }
-
-//     fn write(&mut self, buf: &[u8]) -> Result<u64, common::Error> {
-//         println!("{}", String::from_utf8_lossy(buf).into_owned());
-//         Ok(buf.len().try_into()?)
-//         // futures::executor::block_on(async move {
-//         //     self.0
-//         //         .write(buf)
-//         //         .await
-//         //         .map_err(|err| common::Error::trap(err.into()))
-//         // })
-//     }
-
-//     fn write_zeroes(&mut self, nelem: u64) -> Result<u64, common::Error> {
-//         Ok(nelem)
-//         // futures::executor::block_on(async move {
-//         //     self.0
-//         //         .write_zeroes(nelem)
-//         //         .await
-//         //         .map_err(|err| common::Error::trap(err.into()))
-//         // })
-//     }
-//     fn readable(&self) -> Result<(), common::Error> {
-//         Ok(())
-//     }
-//     fn writable(&self) -> Result<(), common::Error> {
-//         Ok(())
-//     }
-// }
-
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() {
+    run().await.unwrap();
+}
+
+async fn run() -> anyhow::Result<()> {
     let mut table = Table::new();
-    let mut wasi = WasiCtxBuilder::new()
+    let http = WasiHttpCtx::new();
+    let wasi = WasiCtxBuilder::new()
         .inherit_stdio()
-        .push_env(
-            "AWS_API_KEY",
-            &env::var("AWS_API_KEY").expect("AWS_API_KEY is not set"),
-        )
-        .set_args(&["--verbose", "--region", "us-west-2"])
+        .env("RUST_BACKTRACE", "full")
+        .args(&[
+            "s3",
+            "list-objects",
+            "-vvv",
+            "--region",
+            "us-east-2",
+            // "--help"
+            "--bucket",
+            "nara-national-archives-catalog",
+            "--delimiter",
+            "/",
+            "--prefix",
+            "authority-records/organization/",
+            "--max-keys",
+            "5",
+        ])
         .build(&mut table)?;
-    let mut http = WasiHttp::new();
-    // http.set_stdout(Box::new(WritePipe(wasi_common::pipe::WritePipe::new(
-    //     std::io::sink(),
-    // ))));
-    // http.set_stderr(Box::new(WritePipe(wasi_common::pipe::WritePipe::new(
-    //     std::io::sink(),
-    // ))));
     let mut config = Config::new();
     config.wasm_backtrace_details(wasmtime::WasmBacktraceDetails::Enable);
     config.wasm_component_model(true);
@@ -104,25 +70,20 @@ async fn main() -> anyhow::Result<()> {
     config.async_support(true);
 
     let engine = Engine::new(&config)?;
-    let component = Component::from_file(&engine, "../../target/wasm32-wasi/debug/aws_cli.wasm")?;
+    let component = Component::from_file(&engine, "../../target/wasm32-wasi/release/aws_cli.wasm")?;
     let mut store = Store::new(&engine, Ctx { table, wasi, http });
     let mut linker = Linker::<Ctx>::new(&engine);
 
-    add_to_linker(&mut linker)?;
-    wasmtime_wasi_http::add_to_component_linker(&mut linker, |ctx: &mut Ctx| &mut ctx.http)?;
+    wasmtime_wasi::preview2::command::add_to_linker(&mut linker)?;
+    wasmtime_wasi_http::proxy::add_to_linker(&mut linker)?;
 
-    let (wasi, _instance) = Command::instantiate_async(&mut store, &component, &linker).await?;
-    let result = wasi
+    let (command, _instance) = Command::instantiate_async(&mut store, &component, &linker).await?;
+    command
+        .wasi_cli_run()
         .call_run(&mut store)
         .await
-        .map_err(anyhow::Error::from)?;
-
-    if result.is_err() {
-        anyhow::bail!(
-            "command returned with failing exit status: {:?}",
-            result.err().unwrap()
-        );
-    }
+        .map_err(|e| anyhow::anyhow!("wasm failed with {e:?}"))?
+        .map_err(|e| anyhow::anyhow!("command returned with failing exit status {e:?}"))?;
 
     Ok(())
 }
