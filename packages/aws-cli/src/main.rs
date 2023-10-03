9f70f33f30b6b748273d402594b49d3f1276edaa
Eduardo Rodrigues <16357187+eduardomourar@users.noreply.github.com>
2023-10-03 23:20:36 +0200

    chore: use wasi http client from local library

--- packages/aws-cli/src/main.rs
+++ packages/aws-cli/src/main.rs
@@ -1,5 +1,3 @@
-#![cfg(all(target_family = "wasm", target_os = "wasi"))]
-
 cargo_component_bindings::generate!();
 
 mod adapter;
@@ -7,13 +5,11 @@ mod commands;
 mod logger;
 mod s3;
 
-use commands::run;
-
 fn main() {
     crate::logger::init();
     std::panic::set_hook(Box::new(move |panic_info| {
         eprintln!("Internal unhandled panic:\n{:?}!", panic_info);
         std::process::exit(1);
     }));
-    futures::executor::block_on(run());
+    futures::executor::block_on(crate::commands::run());
 }
